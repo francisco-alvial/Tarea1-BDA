4066b834ff35a32a2f2abde0915f80eae157dd25
Francisco Alvial <francisco.alvial.13@sansano.usm.cl>
2019-04-05 13:48:20 -0300

    Dataset y nuevos filtros

--- R_BDA.R
+++ R_BDA.R
@@ -1,29 +1,26 @@
 library(mongolite)
+library(tm)
+library(SnowballC)
 
 c=c=mongo(collection = "EmpresasT" ,db="BDA")
 
 c$count('{}')
 alldata <- c$find('{}')
-print(alldata)
-
-
 
 premium_diamonds <- c$find('{"company" : "google", "price" : { "$lt" : 1000 } }')
 print(premium_diamonds)
 
-
-MyData <- read.csv(file="D:/Descargas/google-amazon-facebook-employee-reviews/employee_reviews.csv", header=TRUE, sep=",")
-
-print(MyData[17])
-
-c$insert(MyData)
-
+MyData <- read.csv(file="C:/Users/franc/Documents/Gitkraken/Tarea1-BDA/employee_reviews.csv", header=TRUE, sep=",")
 
 # se quitaron los index y links
-Datos_columnas_correctas <- read.csv(file="D:/Descargas/google-amazon-facebook-employee-reviews/employee_reviews.csv", header=TRUE, sep=",", colClasses=c("NULL", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, "NULL"))
-print(Datos_columnas_correctas[17])
-c$insert(Datos_columnas_correctas)
+Datos_columnas_correctas <- read.csv(file="C:/Users/franc/Documents/Gitkraken/Tarea1-BDA/employee_reviews.csv", header=TRUE, sep=",", colClasses=c("NULL", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, "NULL"))
 
 #Se hizo todo minuscula
 Datos_columnas_correctas[] <- lapply(Datos_columnas_correctas, tolower)
+Datos_columnas_correctas[] <- lapply(Datos_columnas_correctas, gsub, pattern='-', replacement='')
+Datos_columnas_correctas[] <- lapply(Datos_columnas_correctas, gsub, pattern='*', replacement='')
+Datos_columnas_correctas[] <- lapply(Datos_columnas_correctas, gsub, pattern=':', replacement='')
+Datos_columnas_correctas[] <- lapply(Datos_columnas_correctas, gsub, pattern='[[:punct:]]', replacement='')
 Datos_columnas_correctas[1,]
+
+c$insert(Datos_columnas_correctas)
